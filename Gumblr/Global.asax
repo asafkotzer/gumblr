25dfbfaaaf9254c45f997d08cbf8f71af69e9a40
asafkotzer <asafkotzer@gmail.com>
2014-04-25 16:41:54 +0300

    First commit

---
+++ Gumblr/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Gumblr.MvcApplication" Language="C#" %>
---
+++ Gumblr/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Gumblr.Startup))]
+namespace Gumblr
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
