72b661db396a7272a3e586cc878f25ccd13fd47f
asafkotzer <gumblr@gmail.com>
2014-05-26 11:54:29 -0700

    Added toastr, initial UX for ranking (previous commit failed)

---
+++ Gumblr/Scripts/Home/Home.js
@@ -0,0 +1,7 @@
+﻿var Home = function () {
+    this.init = function () {
+        if (location.search.indexOf("invite=true") > 0) {
+            toastr.success("Waiting for Shachar now", "Your request was submitted");
+        }
+    };
+};
