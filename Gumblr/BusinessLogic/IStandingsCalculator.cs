2ea796941669c05e0372cee2558a58362b53afae
asafkotzer <asafkotzer@gmail.com>
2014-05-28 22:35:02 +0300

    Added StandingsCalculator and tests

--- Gumblr/BusinessLogic/IStandingsCalculator.cs
+++ Gumblr/BusinessLogic/IStandingsCalculator.cs
@@ -8,14 +8,98 @@ namespace Gumblr.BusinessLogic
 {
     public interface IStandingsCalculator
     {
-        Dictionary<string, List<string>> GetStandingsByGroup(IEnumerable<Match> aCompletedMatches, IEnumerable<Match> aStubMatches);
+        Group GetGroupStandings(IEnumerable<Match> aMatches);
+        Group GetGroupStandings(IEnumerable<Match> aMatches, IEnumerable<CoinFlip> aCoinFlips);
     }
 
     public class StandingsCalculator : IStandingsCalculator
     {
-        public Dictionary<string, List<string>> GetStandingsByGroup(IEnumerable<Match> aCompletedMatches, IEnumerable<Match> aStubMatches)
+        public Group GetGroupStandings(IEnumerable<Match> aMatches)
         {
-            throw new NotImplementedException();
+            return GetGroupStandings(aMatches, Enumerable.Empty<CoinFlip>());
+        }
+
+        public Group GetGroupStandings(IEnumerable<Match> aMatches, IEnumerable<CoinFlip> aCoinFlips)
+        {
+            if (aMatches == null) throw new ArgumentNullException("aMatches");
+            if (!aMatches.Any()) throw new ArgumentException("Match list cannot be empty", "aMatches");
+            if (aCoinFlips == null) throw new ArgumentNullException("aCoinFlips");
+
+            var teamByTeamName = new Dictionary<string, Team>();
+            foreach (var match in aMatches)
+            {
+                var host = GetOrCreateTeam(teamByTeamName, match.Host);
+                var visitor = GetOrCreateTeam(teamByTeamName, match.Visitor);
+
+                if (match.GoalsScoredByHost > match.GoalsScoredByVisitor)
+                {
+                    host.Score += 3;
+                }
+                else if (match.GoalsScoredByHost < match.GoalsScoredByVisitor)
+                {
+                    visitor.Score += 3;
+                }
+                else
+                {
+                    host.Score++;
+                    visitor.Score++;
+                }
+
+                host.ScoredGoals += match.GoalsScoredByHost;
+                host.ReceivedGoals += match.GoalsScoredByVisitor;
+
+                visitor.ScoredGoals += match.GoalsScoredByVisitor;
+                visitor.ReceivedGoals += match.GoalsScoredByHost;
+            }
+
+            var orderedList = teamByTeamName.Values
+                .OrderByDescending(x => x.Score)
+                .ThenByDescending(x => x.ScoredGoals - x.ReceivedGoals)
+                .ThenByDescending(x => aCoinFlips.Count(c => c.Winner == x.Name))
+                .ToList();
+
+            var result = new Group { Teams = orderedList };
+            return result;
+        }
+
+        private static Team GetOrCreateTeam(Dictionary<string, Team> teamByTeamName, string aTeamName)
+        {
+            Team team = null;
+            if (!teamByTeamName.TryGetValue(aTeamName, out team))
+            {
+                teamByTeamName[aTeamName] = team = new Team(aTeamName);
+            }
+            return team;
+        }
+    }
+
+    public class Group
+    {
+        public List<Team> Teams { get; set; }
+    }
+
+    public class Team
+    {
+        public string Name { get; set; }
+        public int Score { get; set; }
+        public int ScoredGoals { get; set; }
+        public int ReceivedGoals { get; set; }
+
+        public Team(string aName)
+        {
+            Name = aName;
+        }
+    }
+
+    public class CoinFlip
+    {
+        public string Winner { get; set; }
+        public string Loser { get; set; }
+
+        public CoinFlip(string aWinner, string aLoser)
+        {
+            Winner = aWinner;
+            Loser = aLoser;
         }
     }
 }
---
+++ GumblrUnitTests/StandingsCalculatorTests.cs
@@ -0,0 +1,125 @@
+﻿using Gumblr.BusinessLogic;
+using Gumblr.Models;
+using MsTest = Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GumblrUnitTests
+{
+    [TestFixture]
+    [MsTest.TestClass]
+    public class StandingsCalculatorTests
+    {
+        [Test]
+        public void GetStandingsByGroup_NullList_ThrowsNullArgumentException()
+        {
+            var calculator = new StandingsCalculator();
+
+            Assert.Throws<ArgumentNullException>(() => calculator.GetGroupStandings(null));
+        }
+
+        [Test]
+        public void GetStandingsByGroup_EmptyList_ReturnsEmptyResult()
+        {
+            var calculator = new StandingsCalculator();
+
+            Assert.Throws<ArgumentException>(() => calculator.GetGroupStandings(new List<Match>()));
+        }
+
+        [Test]
+        public void GetStandingsByGroup_ListWithOneMatch_ReturnsOneGroupWithTwoTeams()
+        {
+            var calculator = new StandingsCalculator();
+            var matches = new List<Match>()
+            {
+                new Match { Host = "host", Visitor = "visitor", Stage = MatchStage.Group, Group = "A", GoalsScoredByHost = 0, GoalsScoredByVisitor = 0 }
+            };
+
+            var result = calculator.GetGroupStandings(matches);
+
+            Assert.AreEqual(2, result.Teams.Count());
+        }
+
+        [Test]
+        public void GetStandingsByGroup_ListWithOneMatchWhereHostWins_CorrectStandings()
+        {
+            var calculator = new StandingsCalculator();
+            var matches = new List<Match>()
+            {
+                new Match { Host = "host", Visitor = "visitor", Stage = MatchStage.Group, Group = "A", GoalsScoredByHost = 1, GoalsScoredByVisitor = 0 }
+            };
+
+            var result = calculator.GetGroupStandings(matches);
+
+            Assert.AreEqual("host", result.Teams.First().Name);
+            Assert.AreEqual(3, result.Teams.First().Score);
+            Assert.AreEqual(1, result.Teams.First().ScoredGoals);
+            Assert.AreEqual(0, result.Teams.First().ReceivedGoals);
+
+            Assert.AreEqual("visitor", result.Teams.Last().Name);
+            Assert.AreEqual(0, result.Teams.Last().Score);
+            Assert.AreEqual(0, result.Teams.Last().ScoredGoals);
+            Assert.AreEqual(1, result.Teams.Last().ReceivedGoals);
+        }
+
+        [Test]
+        public void GetStandingsByGroup_OneMatch_Tie_CorrectStandings()
+        {
+            var calculator = new StandingsCalculator();
+            var matches = new List<Match>()
+            {
+                new Match { Host = "host", Visitor = "visitor", Stage = MatchStage.Group, Group = "A", GoalsScoredByHost = 1, GoalsScoredByVisitor = 0 }
+            };
+
+            var result = calculator.GetGroupStandings(matches);
+
+            Assert.AreEqual("host", result.Teams.First().Name);
+            Assert.AreEqual(3, result.Teams.First().Score);
+            Assert.AreEqual(1, result.Teams.First().ScoredGoals);
+            Assert.AreEqual(0, result.Teams.First().ReceivedGoals);
+
+            Assert.AreEqual("visitor", result.Teams.Last().Name);
+            Assert.AreEqual(0, result.Teams.Last().Score);
+            Assert.AreEqual(0, result.Teams.Last().ScoredGoals);
+            Assert.AreEqual(1, result.Teams.Last().ReceivedGoals);
+        }
+
+        [TestCase]
+        public void GetStandingsByGroup_TwoMacthesFourTeams_TwoTeamsWithSameScoreDifferentGoalCount_CorrectScore()
+        {
+            var calculator = new StandingsCalculator();
+            var matches = new List<Match>()
+            {
+                new Match { Host = "team1", Visitor = "team2", GoalsScoredByHost = 2, GoalsScoredByVisitor = 0 },
+                new Match { Host = "team3", Visitor = "team4", GoalsScoredByHost = 1, GoalsScoredByVisitor = 0 },
+            };
+
+            var result = calculator.GetGroupStandings(matches);
+
+            Assert.AreEqual("team1", result.Teams[0].Name);
+            Assert.AreEqual("team3", result.Teams[1].Name);
+            Assert.AreEqual("team4", result.Teams[2].Name);
+            Assert.AreEqual("team2", result.Teams[3].Name);
+        }
+
+        [TestCase]
+        public void GetStandingsByGroup_OneMacthTie_CoinFlip_CorrectScore()
+        {
+            var calculator = new StandingsCalculator();
+            var matches = new List<Match>()
+            {
+                new Match { Host = "team1", Visitor = "team2", GoalsScoredByHost = 0, GoalsScoredByVisitor = 0 },
+            };
+
+            var result = calculator.GetGroupStandings(matches, new List<CoinFlip> { new CoinFlip("team2", "team1") });
+
+            Assert.AreEqual("team2", result.Teams.First().Name);
+            Assert.AreEqual("team1", result.Teams.Last().Name);
+        }
+
+    }
+}
